2cf90b59ccebe8e5bc555641c331e57c8d97dce9
PainTrap <mamasboy199329@gmail.com>
2024-01-14 19:51:56 +0100

    updates

--- scriptb.js
+++ scriptb.js
@@ -3,7 +3,11 @@ let day = 14;
 let year = 2024;
 let hour;
 let minute;
-let second;
+let second = 0;
 let date = document.getElementById('time');
 date = {month,  day,  year};
 
+setInterval(function(){
+    second++;
+}, 1000)
+ second = document.getElementById('time')
