143e5595de0ce259d6f129d9b501aa400d3bbd00
PainTrap <mamasboy199329@gmail.com>
2024-01-14 14:13:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,4 @@ let year = 2024;
 let hour;
 let minute;
 let second;
-document.getElementById('time');{
-    document.write(month,'/',day, '/', year)
-}
+document.getElementById('time').write(month,'/',day, '/', year)
